b37dbe948f69cd4a02987e36ffa7cd30424eaecb
yamig <165082448+yamig@users.noreply.github.com>
2026-09-17 18:20:18 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     try {
 
         const response = await fetch(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.8-flash:generateContent",
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent",
             {
                 method: "POST",
 
